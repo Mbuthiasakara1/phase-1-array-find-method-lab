a37716b1b17dd1633fd543d8e4ab706dc90b4053
Mbuthiasakara1 <mbuthiaryne@gmail.com>
2024-07-01 15:19:00 +0300

    completes the lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(record) {
+  const winningRecord = record.find(function (element) {
+    return element.result === "W";
+  });
+
+  if (winningRecord) {
+    return winningRecord.year;
+  } else {
+    return undefined;
+  }
+}
